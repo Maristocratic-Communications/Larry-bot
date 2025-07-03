98bc23d3249c5b24a0bb7772a846b2df32974272
Mari <pentium-m@dmc.chat>
2025-07-03 19:08:45 -0400

    now it can login to discord wow

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 require('dotenv').config()
-console.log("preparing...")
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 const token = process.env.TOKEN
+console.log("preparing...")
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
 if (typeof token == 'undefined') {
   console.log("Your Token is missing inside of .env\nplease copy .env_example and imput your token from the developer portal.");
   process.exit(1);
 }
-console.log(token)
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
