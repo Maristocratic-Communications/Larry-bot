0c1eee754c351492453546e02427a0129a40fd0f
Mari <pentium-m@dmc.chat>
2025-07-03 18:54:45 -0400

    development

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+const fs = require('fs');
 require('dotenv').config()
 console.log("preparing...")
 const token = process.env.TOKEN
+if (typeof token == 'undefined') {
+  console.log("Your Token is missing inside of .env\nplease copy .env_example and imput your token from the developer portal.");
+  process.exit(1);
+}
 console.log(token)
