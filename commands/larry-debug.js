7e256d5cb4a2fdfec0c9b4e79b60283cf4c7f2eb
Mari <pentium-m@dmc.chat>
2025-07-03 20:25:29 -0400

    larry.

--- commands/larry-debug.js
+++ commands/larry-debug.js
@@ -1,10 +1,26 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('./larry.json', 'utf8'));
+const entries = Object.entries(data);
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('larry-debug')
 		.setDescription('larry.'),
 	async execute(interaction) {
-		await interaction.reply('larry.');
+        const key = randomLarry()
+		await interaction.reply(`${key.emoji} ${key} has... NOT spawned.`);
 	},
 };
+
+function randomLarry() {
+const totalWeight = entries.reduce((sum, [_, value]) => sum + value.weight, 0);
+const rand = Math.random() * totalWeight;
+let cumulative = 0;
+for (const [key, value] of entries) {
+  cumulative += value.weight;
+  if (rand < cumulative) {
+    return key
+  }
+}}
+
