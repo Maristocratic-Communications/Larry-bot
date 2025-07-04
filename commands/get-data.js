2e5f1a572b467b2b9b6988d1e863159711851a15
Mari <pentium-m@dmc.chat>
2025-07-03 23:29:31 -0400

    i did something i think

---
+++ commands/get-data.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('get-data')
+		.setDescription('outputs server dats'),
+	async execute(interaction) {
+        const data = JSON.parse(fs.readFileSync(`./data/{interaction.guild.id}.json`, 'utf8'));
+		await interaction.reply(`done.`);
+	},
+};
