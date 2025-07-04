501dfe29d955fe51cdb1c7454a319e11df8e2a86
Mari <pentium-m@dmc.chat>
2025-07-03 23:33:06 -0400

    i sure do love storing shittons of files

--- commands/get-data.js
+++ commands/get-data.js
@@ -6,7 +6,19 @@ module.exports = {
 		.setName('get-data')
 		.setDescription('outputs server dats'),
 	async execute(interaction) {
+        createFileIfNotExists(`./data/${interaction.guild.id}.json`, '{}');
         const data = JSON.parse(fs.readFileSync(`./data/${interaction.guild.id}.json`, 'utf8'));
 		await interaction.reply(`done.`);
 	},
 };
+
+async function createFileIfNotExists(path, content = '') {
+  try {
+    await fs.access(path);
+  } catch {
+    await fs.writeFile(path, content);
+    console.log('Data File created:', path);
+  }
+}
+
+
